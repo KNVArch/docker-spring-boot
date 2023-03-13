45017c22a0e9d40875c93e72463b241a1a6330f5
KNVArch <100133977+KNVArch@users.noreply.github.com>
2023-03-13 17:06:11 -0400

    Update docker_installation.sh
    added yes tag(-y) to docker installation

--- docker_installation.sh
+++ docker_installation.sh
@@ -6,7 +6,7 @@
 sudo yum update -y
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo sytemctl enable docker
 
