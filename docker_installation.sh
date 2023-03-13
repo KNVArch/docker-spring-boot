d73828d45331d43405a296a282389bedc98ab5f6
archana <archanaknv@gmail.com>
2023-03-13 17:04:15 -0400

    Added bash scripting file

---
+++ docker_installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Date March 13th 2023
+#Author Archana
+#Purpose docker installation
+
+sudo yum update -y
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo sytemctl enable docker
+
+#END
